edb0411ced9f343bbe33bb9b535ce8651b3966ab
adamjoelblake <adamjoelblake@gmail.com>
2024-11-03 13:36:04 -0700

    d

--- app.py
+++ app.py
@@ -189,8 +189,7 @@ def download_audio():
                     
                     if response.status_code == 200:
                         try:
-                            temp_buffer = BytesIO(response.content)
-                            zip_file.writestr(f"{bookDict['title']}_{index}.mp3", temp_buffer.getvalue())
+                            zip_file.writestr(f"{bookDict['title']}_{index}.mp3", response.content)
                             cloud_logger.info(f"Successfully downloaded file {index}")
                         except Exception as e:
                             cloud_logger.error(f"Failed to write file {index} to ZIP: {e}", exc_info=True)
