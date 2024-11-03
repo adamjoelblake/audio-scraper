67464b5d9341223dd61ba4938ffd92ef27bc65ed
adamjoelblake <adamjoelblake@gmail.com>
2024-11-03 13:00:55 -0700

    d

--- app.py
+++ app.py
@@ -183,7 +183,7 @@ def download_audio():
                 try:
                     for index, file_url in audioFiles.items():
                         # download each audio file
-                        response = requests.get(file_url, stream=True, timeout=30)
+                        response = requests.get(file_url, stream=True, timeout=60)
                         cloud_logger.info(f"File {index} response status: {response.status_code}")
                         if response.status_code == 200:
                             zip_file.writestr(f"{bookDict['title']}_{index}.mp3", response.content)
